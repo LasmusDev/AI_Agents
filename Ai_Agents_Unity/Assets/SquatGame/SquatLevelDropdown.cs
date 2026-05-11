dc95a7c83c7aecf50cf44f1e1fdb9d25df42fc4d
ahmedsey <117659668+ahmedsey@users.noreply.github.com>
2026-05-11 09:26:46 +0200

    Squat Game Dropdown and Teleport Area fix
    This update adds the dropdown to the squat game. Also, the Teleport Area in the Yoga Level got an update

---
+++ Ai_Agents_Unity/Assets/SquatGame/SquatLevelDropdown.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+namespace SquatGame
+{
+    [System.Serializable]
+    public class SquatLevel
+    {
+        public string levelName = "New Level";
+        public SquatPoseMap poseMap;
+        public AudioClip song;
+    }
+
+    public class SquatLevelDropdown : MonoBehaviour
+    {
+        [Tooltip("Drag your SquatManager here")]
+        public SquatManager manager;
+        
+        [Tooltip("Drag your Dropdown here")]
+        public TMP_Dropdown dropdown;
+
+        [Tooltip("Add Levels")]
+        public List<SquatLevel> availableLevels;
+
+        void Start()
+        {
+            if (manager == null || dropdown == null || availableLevels.Count == 0)
+            {
+                Debug.LogWarning("Missing references ");
+                return;
+            }
+
+            dropdown.ClearOptions();
+            List<string> levelNames = new List<string>();
+            foreach (SquatLevel level in availableLevels)
+            {
+                levelNames.Add(level.levelName); 
+            }
+            
+            dropdown.AddOptions(levelNames);
+            dropdown.onValueChanged.AddListener(OnLevelSelected);
+            dropdown.value = 0;
+            OnLevelSelected(0);
+        }
+
+        void Update()
+        {
+            // Set dropdown back to active if the start button is active again
+            if (manager != null && manager.startButton != null)
+            {
+                if (!dropdown.gameObject.activeSelf && manager.startButton.activeSelf)
+                {
+                    dropdown.gameObject.SetActive(true); 
+                }
+            }
+        }
+
+        public void OnLevelSelected(int index)
+        {
+            if (manager != null && manager.isRunning) 
+            {
+                Debug.Log("Game is running. Cannot change level now.");
+                return;
+            }
+            if (index >= 0 && index < availableLevels.Count)
+            {
+                manager.currentPoseMap = availableLevels[index].poseMap;
+                manager.gameMusic = availableLevels[index].song;
+                Debug.Log("New level loaded into SquatManager: " + availableLevels[index].levelName);
+            }
+        }
+    }
+}
